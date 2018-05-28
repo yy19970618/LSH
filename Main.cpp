fe99566890edc083be666507fb481a3ad458ce3a
yy19970618 <745628661@qq.com>
2018-05-28 16:16:57 +0800

    合并

--- Main.cpp
+++ Main.cpp
@@ -11,7 +11,6 @@ const string QUERY_FILE_NAME = ""; //查询集
 
 const int NUM_HASH_TABLES = 50; //哈希表个数
 const int NUM_HASH_BITS = 18; //初始k值
-const ifstream files[2048]; //2048个输出流
 
 
 void processData() //处理数据集的向量,让它们分配到每个桶里去
@@ -19,19 +18,20 @@ void processData() //处理数据集的向量,让它们分配到每个桶里去
 	ifstream *file;
 	file->open(DATA_FILE_NAME, ios::binary);
 	Point *p;
-	Buckets::read_point(file, p);
-	int bucket = hash_helpers::findMax(p->value);
+	while (true)
+	{
+		if (file->peek() == EOF)
+			return ;
+		Buckets::read_point(file, p);
+
+		int bucket = hash_helpers::findMax(p->value);
+		ofstream ofile;
+		string filename = "D:\\1\\buckets\\" + to_string(bucket) + ".binary";
+		ofile.open(filename, ios::binary);
+	}
 
 }
-void init()
-{
-	string filename;
-	for (int i = 1; i <= 2048; i++)
-	{
-		filename = "D:\\vs2012\\projects\\file\\file\\wdata\\" + std::to_string(i) + ".binary";
-		//files[i].open(filename, ios::binary);
-	}
-}
+
 int main() {
 	
 
