608c55045c2b456ef0eb1c198337bd721d7a1626
yy19970618 <745628661@qq.com>
2018-06-01 10:34:26 +0800

    center2

--- Main.cpp
+++ Main.cpp
@@ -1,18 +1,18 @@
 #include "Buckets.h"
 #include "Math.h"
 #include "hash_helpers.h"
-//#include "StaticTable.h"
+#include "StaticTable.h"
 #include <ctime>
 #include <fstream>
 #include <iostream>
 
 using namespace std;
 
-const string DATA_FILE_NAME = "d:/1/1/1.binary"; //数据集
-const string BUCKETS_FILES = "d:/1/buckets/"; //存放桶文件
-const string QUERY_FILE_NAME = "d:/1/1/2.binary"; //查询集
+const string DATA_FILE_NAME = "/media/sunny/source/1/1/1.binary"; //数据集
+const string BUCKETS_FILES = "/media/sunny/source/1/buckets/"; //存放桶文件
+const string QUERY_FILE_NAME = "/media/sunny/source/1/1/2.binary"; //查询集
 //g++ -I ~/home/LSH/include -std=c++11 Main.cpp -g -o main
-const string CENTER_FILE_NAME = "d:/1/1/center.binary";//中心化数据集
+const string CENTER_FILE_NAME = "/media/sunny/source/1/1/center.binary";//中心化数据集
 
 int result[100];//结果集，返回id集
 const int LOG_DIM = 10;
@@ -58,6 +58,89 @@ void queryData()
 
 	}
 }
+void processData() //处理数据集的向量,让它们分配到每个桶里去
+{
+	ifstream *file = new ifstream();
+	ofstream ofile;
+	file->open(DATA_FILE_NAME, ios::binary);
+	if (file->is_open() == false) {
+		cout << "dataset out!"; return;
+	}
+	ifstream cenfile;
+	cenfile.open(CENTER_FILE_NAME, ios::binary);
+	for(int i=0;i<1024;i++)
+		cenfile.read((char*)&(center[i]), sizeof(center[i]));
+	/*ofstream ofile[2048];
+	for (int i = 1; i <= 2048; i++) {
+		string temp = BUCKETS_FILES + to_string(i) + ".binary";
+		cout << i << endl;
+		ofile[i-1].open(temp, ios::binary | ios::app);
+		if (ofile[i - 1].is_open() == false) {
+			cout << "file out!"; return;
+		}
+	}*/
+	
+	Point p ;
+	int bucket;
+	time_t now = time(0);
+	tm *ltm = localtime(&now);
+	int hour = ltm->tm_hour;
+	int min = ltm->tm_min;
+	int sec = ltm->tm_sec;
+	while (true)
+	{
+		if (file->peek() == EOF)
+		{
+			file->close();
+			time_t now1 = time(0);
+			tm *ltm1 = localtime(&now1);
+			cout << "start time:" << ":" << hour << ":" << min << ":" << sec<<endl;
+			cout << "end time:" << ltm1->tm_hour << ":" << ltm1->tm_min << ":" << ltm1->tm_sec << endl;
+			return;
+		}
+		Buckets::read_point(file, &p);
+		float vector[1024];
+		for (int i = 0; i < 1024; i++) {
+			vector[i] = p.value[i];
+		}
+		
+		/*cout<<"a";
+		for(int i=0;i<1024;i++)
+			cout<<vector[i]<<" ";
+		cout<<endl;*/
+
+		hash_helpers::pointUnit(vector);
+
+		/*cout<<"b";
+		for(int i=0;i<1024;i++)
+			cout<<vector[i]<<" ";
+		cout<<endl;*/
+
+		for(int i=0;i<1024;i++)
+			vector[i]=vector[i]-center[i];
+
+		/*cout<<"c";
+		for(int i=0;i<1024;i++)
+			cout<<vector[i]<<" ";
+		cout<<endl;*/
+
+		int bucket[k];
+		StaticTable::random_rotate(vector, LOG_DIM,k,d,bucket);
+
+		string filename = BUCKETS_FILES;
+		char buffer[20];
+		for (int i = 0; i < k; i++)
+		{
+			sprintf(buffer, "%04d", bucket[i]);
+			filename = filename + buffer;
+		}
+		filename = filename + ".binary";
+		ofile.open(filename, ios::binary | ios::app);
+		Buckets::writePoint(&p, &ofile);
+		ofile.close();
+		cout << p.id << endl;
+	}
+}
 void countCenter() {
 	ifstream *file = new ifstream();
 	file->open(DATA_FILE_NAME, ios::binary);
@@ -95,35 +178,6 @@ void countCenter() {
 int main() {
 	//countCenter();
 	//processData();
-	Point p[4];
-	ifstream *file = new ifstream();
-	file->open("d:/1/buckets/09631376.binary", ios::binary);
-	for(int i=0;i<4;i++){
-		Buckets::read_point(file, &p[i]);
-	}
-	file->close();
-	for(int j=1;j<4;j++)
-	{
-		float count=0;
-		for(int i=0;i<1024;i++)
-		{
-			count=count+(p[0].value[i]-p[j].value[i])*(p[0].value[i]-p[j].value[i]);
-		}
-		cout<<count<<endl;
-	}
-	file->open(DATA_FILE_NAME, ios::binary);
-	for (int i = 0; i<4; i++) {
-		Buckets::read_point(file, &p[i]);
-	}
-	for (int j = 1; j<4; j++)
-	{
-		float count = 0;
-		for (int i = 0; i<1024; i++)
-		{
-			count = count + (p[0].value[i] - p[j].value[i])*(p[0].value[i] - p[j].value[i]);
-		}
-		cout << count << endl;
-	}
 
 	return 0;
 }
